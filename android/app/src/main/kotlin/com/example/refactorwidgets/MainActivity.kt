8f57644e50883a60d734a9cc5a41eb6764b42b4d
ramadan <ahmed.201317499@gmail.com>
2022-12-21 21:42:35 +0200

    Create The App

---
+++ android/app/src/main/kotlin/com/example/refactorwidgets/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.refactorwidgets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const RefactorWidgets());
+}
+
+class RefactorWidgets extends StatelessWidget {
+  const RefactorWidgets({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      
+    );
+  }
+}
+
